33fb7b18c824fff8da15d14708bbfbb280f11113
Gregory Presser <gpress2222@gmail.com>
2022-03-14 20:36:41 -0400

    Got it working and did the output

--- CKYpare.py
+++ CKYpare.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# from functools import map
+from functools import reduce
 from collections import defaultdict
 from nltk.tokenize import word_tokenize
 from nltk import download as n_download
@@ -37,45 +37,73 @@ class Table:
     def __init__(self, grammer: Grammer, sentence: str) -> None:
         self.words = word_tokenize(sentence)
         self.grammer = grammer
+        self.wc = len(self.words)
+        self.table = [[[] for _ in range(self.wc)] for _ in range(self.wc)]
 
-        wc = len(self.words)
 
-        self.table = [[[] for _ in range(wc)] for _ in range(wc)]
+        # populate terminals 
         for i,word in enumerate(self.words):
             self.table[i][i] = grammer.terminal[word]
 
-        # loop through diagonals
-        for i in range(1,wc):
-            self.__check_diagnal(i)
+        # loop through diagonals and populate non_terminals 
+        for gsdx in range(1,self.wc): 
+            for sdy in range(0,self.wc-gsdx):
+                sdx = gsdx+sdy
+                for dx in range(gsdx,0,-1):
+                    # dy is the compliment 
+                    dy = (gsdx+1)-dx
+                    for combo,fw,i_f,sw,i_s in Table.__make_strings(self.table[sdy][sdx-dx], self.table[sdy+dy][sdx]): 
+                        self.table[sdy][sdx] += map(
+                            lambda val: (val,(sdy,sdx-dx,fw,i_f), (sdy+dy,sdx,sw,i_s)) # adds backpointers
+                            ,self.grammer.non_terminal[combo])
     
-    def __check_diagnal(self,d_loc:int):
-        seconds = [(self.table[d_loc][d_loc],d_loc,d_loc)]
-        for y in range(d_loc-1,-1,-1):
-            for x in range(d_loc-1,-1,-1):
-                if len(self.table[y][x]) == 0: continue
-                second_adder = []
-                for second,ly,lx in seconds:
-                    for combo,fw,sw in Table.__make_strings(self.table[y][x],second):
-                        if combo in self.grammer.non_terminal:
-                            self.table[ly][d_loc] += map(
-                                lambda v: (v,(y,x,fw),(ly,lx,sw)),
-                                self.grammer.non_terminal[combo])
-                            second_adder.append(( self.table[ly][d_loc][0],ly,d_loc))
-                seconds += second_adder 
+
+    def outputPermutations(self,tree_mode: bool = False) -> None: 
+        S_arr = [val for val in self.table[0][self.wc-1] if val[0] == "S"]
+        if len(S_arr) <= 0:
+            print("Invalid Sentence")
+            return
+
+        newline = '\n'
+
+        def __make_bracket(ty:int,tx:int,tz:int,tab_amount:int = 1) -> str:
+            if not tree_mode:
+                tab_amount = 0
+
+            if ty == tx:
+                return f"{' '*tab_amount}[{self.table[ty][tx][tz]} {self.words[ty]}]"
+            (name,f,s) = self.table[ty][tx][tz]
+            return f"{' '*tab_amount}[{name}{newline if tree_mode else ' '}{__make_bracket(f[0],f[1],f[3],tab_amount+1)}{newline if tree_mode else ' '}{__make_bracket(s[0],s[1],s[3],tab_amount+1)}{newline + (' '*tab_amount) if tree_mode else ''}]"
+
+        for parse_count,(_,first,second) in enumerate(S_arr):
+            print(f"Valid parse #{parse_count+1}:")
+            print(f"[S{newline if tree_mode else ' '}{__make_bracket(first[0],first[1],first[3])}{newline if tree_mode else ' '}{__make_bracket(second[0],second[1],second[3])}{newline if tree_mode else ''}]")
 
     @staticmethod 
     def __make_strings(first,second):
-        for fw in first:
-            for sw in second:
-                yield f"{fw} {sw}",fw,sw
+        first = [val if not isinstance(val,tuple) else val[0] for val in first]
+        second = [val if not isinstance(val,tuple) else val[0] for val in second]
+        for i_f,fw in enumerate(first):
+            for i_s,sw in enumerate(second):
+                yield f"{fw} {sw}",fw,i_f,sw,i_s
 
 def get_input() -> str:
     return input('Input a sentence\ntype "quit" to quit the application\n')
 
 if __name__ == "__main__":
+    print("Loading Grammer...")
     grammer = Grammer.make_grammer("sampleGrammar.cnf")
+    
+    val = input("Do you want textual parse trees to be displayed (y/n)?:")
+    while val != 'y' and val != 'n':
+        val = input("Do you want textual parse trees to be displayed (y/n)?:")
+    val = val == 'y'
     sentence = get_input()
     while sentence != "quit":
         t = Table(grammer,sentence)
+        if val:
+            t.outputPermutations(True)
+        t.outputPermutations()
         sentence = get_input()
+    print("Goodbye!")
 
